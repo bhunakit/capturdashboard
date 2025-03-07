b01bb59c411e6953727769ac030a0e611ef4e137
bhunakit <bhunakit@gmail.com>
2025-03-07 19:11:52 +0700

    side panel retitle

--- src/App.jsx
+++ src/App.jsx
@@ -203,7 +203,7 @@ function App() {
             </button>
             <div className="header-content">
               <div className="camera-tag">Camera #{selectedCamera?.id}</div>
-              <h2>CCTV Analytics</h2>
+              <h2>Captur Analytics</h2>
               <p className="location-text">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
